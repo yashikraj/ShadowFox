64216e405d2c0c538825a1e964459f15af30b757
Yashik Raj <sb0200916@gmail.com>
2025-08-22 16:15:30 +0530

    numbers.py

---
+++ Task1_Beginner/numbers.py
@@ -0,0 +1,15 @@
+formatted = format(145, 'o')
+print("Formatted (octal) value of 145:", formatted)
+
+radius = 84
+area = 3.14 * (radius ** 2)
+print("Area of pond:", int(area))
+
+water = int(area * 1.4)
+print("Total water in pond (liters):", water)
+
+distance = 490
+time_minutes = 7
+time_seconds = time_minutes * 60
+speed = distance / time_seconds
+print("Speed in m/s:", int(speed))
