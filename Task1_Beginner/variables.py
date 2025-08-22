8cb3868bda2d4ba5fa6167347aca37ca12922afa
Yashik Raj <sb0200916@gmail.com>
2025-08-22 16:06:55 +0530

    Create variables.py

---
+++ Task1_Beginner/variables.py
@@ -0,0 +1,14 @@
+pi = 22/7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+try:
+    for = 4
+except:
+    print("Cannot use 'for' as a variable name because it is a reserved keyword in Python.")
+
+P = 10000
+R = 5
+T = 3
+SI = (P * R * T) / 100
+print("Simple Interest for 3 years:", SI)
